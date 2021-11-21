157b6aab44a78d0c0a965c2a717039961a96acc6
lijie <lijie37927@outlook.com>
2021-11-21 22:45:00 +0800

    优化

--- app/src/main/java/com/lijie/jiancang/ui/compose/ImeScroll.kt
+++ app/src/main/java/com/lijie/jiancang/ui/compose/ImeScroll.kt
@@ -3,53 +3,27 @@ package com.lijie.jiancang.ui.compose
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.runtime.*
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsAnimationCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.accompanist.insets.LocalWindowInsets
 
 
 var selectionTop by mutableStateOf(0)
-private var isVisible by mutableStateOf(false)
-private var imeBottom by mutableStateOf(0)
-private var maxImeBottom by mutableStateOf(0)
 
 fun Modifier.imeScroll(height: Int, scrollState: ScrollState) = composed(
     factory = {
-        val view = LocalView.current
-        DisposableEffect(view) {
-            ViewCompat.setWindowInsetsAnimationCallback(view, object :
-                WindowInsetsAnimationCompat.Callback(DISPATCH_MODE_STOP) {
-
-                override fun onProgress(
-                    wic: WindowInsetsCompat,
-                    runningAnimations: MutableList<WindowInsetsAnimationCompat>
-                ): WindowInsetsCompat {
-                    imeBottom = wic.getInsets(WindowInsetsCompat.Type.ime()).bottom
-                    return wic
-                }
-            })
-            onDispose {
-                selectionTop = 0
-                isVisible = false
-                imeBottom = 0
-                maxImeBottom = 0
-            }
-        }
-        if (maxImeBottom < imeBottom) {
-            maxImeBottom = imeBottom
-        }
+        val ime = LocalWindowInsets.current.ime
         val scrollValue = scrollState.value
-        isVisible = imeBottom != 0
-        if (isVisible) {
-            if ((selectionTop - scrollValue) > maxImeBottom) {
-                val to = (maxImeBottom - (height - (selectionTop - scrollValue))) + 200
+        if (ime.isVisible) {
+            if ((selectionTop - scrollValue) > ime.bottom) {
+                val to = (ime.bottom - (height - (selectionTop - scrollValue))) + 200
                 if (to > 0) {
-                    LaunchedEffect(imeBottom) {
+                    LaunchedEffect(ime.bottom) {
                         scrollState.scrollTo(
                             (scrollState.value + to)
                         )
@@ -63,7 +37,8 @@ fun Modifier.imeScroll(height: Int, scrollState: ScrollState) = composed(
 
 fun Modifier.imeHeight() = composed(
     factory = {
-        val imeBottomDb = with(LocalDensity.current) { imeBottom.toDp() }
+        val ime = LocalWindowInsets.current.ime
+        val imeBottomDb = with(LocalDensity.current) { ime.bottom.toDp() }
         this.height(imeBottomDb)
     }
 )
